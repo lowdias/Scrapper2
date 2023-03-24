9dbaeab2a4d6dda960a609ae062b1bf276b2dbad
lowdias <ilias.kamal@gmail.com>
2023-03-24 15:26:30 +0000

    Initial commit

---
+++ Scrappy_beauty.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+import sqlite3
+
+# Define the URL of the news website to scrape
+url = "https://www.bbc.com/news"
+
+# Make a request to the website
+response = requests.get(url)
+
+# Parse the HTML content of the website using BeautifulSoup
+soup = BeautifulSoup(response.content, "html.parser")
+
+# Find all the articles on the website
+articles = soup.find_all("article")
+
+# Connect to the SQLite database and create a table to store the articles
+conn = sqlite3.connect("news_articles.db")
+c = conn.cursor()
+c.execute("CREATE TABLE IF NOT EXISTS articles (title TEXT, summary TEXT, url TEXT)")
+
+# Loop through the articles and extract the title, summary, and URL
+for article in articles:
+    title = article.find("h3").text.strip()
+    summary = article.find("p").text.strip()
+    url = article.find("a")["href"]
+
+    # Insert the data into the SQLite database
+    c.execute("INSERT INTO articles (title, summary, url) VALUES (?, ?, ?)", (title, summary, url))
+
+# Commit the changes to the database and close the connection
+conn.commit()
+conn.close()
---
+++ scrappy_2.py
@@ -0,0 +1,58 @@
+import scrapy
+import sqlite3
+
+
+class BBCSpider(scrapy.Spider):
+    name = "bbc"
+    start_urls = [
+        "https://www.reuters.com/world/",
+    ]
+
+    def __init__(self):
+        self.connection = sqlite3.connect("articles.db")
+        print(sqlite3.version)
+        self.cursor = self.connection.cursor()
+
+        # Create a table to store the articles
+        self.cursor.execute("""CREATE TABLE IF NOT EXISTS articles (
+            id INTEGER PRIMARY KEY,
+            title TEXT,
+            author TEXT,
+            date TEXT,
+            content TEXT,
+            url TEXT
+        )""")
+
+    def parse(self, response):
+        # Extract article links from the homepage
+        for article_link in response.css("a.gs-c-promo-heading.gs-o-faux-block-link__overlay-link::attr(href)"):
+            yield response.follow(article_link, self.parse_article)
+
+        # Follow links to additional pages of articles
+        for next_page in response.css("a.gs-c-pagination-button.gs-c-pagination-button--next.gs-o-button::attr(href)"):
+            yield response.follow(next_page, self.parse)
+
+    def parse_article(self, response):
+        # Extract the article title, author, date, and content
+        title = response.css("h1.story-body__h1::text").get()
+        author = response.css("div.byline__name::text").get()
+        date = response.css("div.date::text").get()
+        content = "\n".join(response.css("div.story-body__inner p::text").getall())
+
+        # Store the extracted data in the database
+        sql = "INSERT INTO articles (title, author, date, content, url) VALUES (?, ?, ?, ?, ?)"
+        values = (title, author, date, content, response.url)
+        self.cursor.execute(sql, values)
+        self.connection.commit()
+
+        # Return the extracted data as a dictionary (optional)
+        yield {
+            "title": title,
+            "author": author,
+            "date": date,
+            "content": content,
+            "url": response.url,
+        }
+
+    def closed(self, reason):
+        self.connection.close()
